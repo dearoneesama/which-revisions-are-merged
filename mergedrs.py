9aad8591b84382f13d2088a04682140434ff9cb2
dearoneesama <2333@outlook.jp>
2020-04-13 15:18:50 -0400

    oo

--- mergedrs.py
+++ mergedrs.py
@@ -1,18 +1,15 @@
-# you can provide an argument like 1-40 to only extract revisions r1 to r-40, inclusive
+#!/usr/bin/env python3
+# you can provide an argument like `-r 1-40` to only extract revisions r1 to r-40, inclusive
 # or default scan all revisions if no argument is provided
 
+import argparse
 import functools
 import pathlib
 import re
 import subprocess
-import sys
 from typing import List, NamedTuple, Optional
 
-
-SVN = 'svn'
-ROOT_URL = ''
-
-OUTDIR = pathlib.Path('mergedrs.out')
+run_command = functools.partial(subprocess.run, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 
 class Mergeinfo(NamedTuple):
@@ -21,65 +18,77 @@ class Mergeinfo(NamedTuple):
     revend: int   # inclusive
 
 
-run_command = functools.partial(subprocess.run, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+class Program:
+
+    def __init__(self, svn: str, rooturl: str):
+        self.SVN = svn
+        self.ROOT_URL = rooturl
+        self.OUTDIR = pathlib.Path('mergedrs.out')
+
+    def get_last_rev_number(self) -> int:
+        res = run_command([self.SVN, 'info', '--show-item', 'revision', self.ROOT_URL])
+        if res.returncode != 0:
+            print('cannot get last revision.')
+            exit(1)
+        return int(res.stdout.strip(b'\n'))
 
 
-def get_last_rev_number() -> int:
-    res = run_command([SVN, 'info', '--show-item', 'revision', ROOT_URL])
-    if res.returncode != 0:
-        print('cannot get last revision.')
-        exit(1)
-    return int(res.stdout.strip(b'\n'))
+    def get_mergeinfo(self, revision: int) -> Optional[Mergeinfo]:
+        proc = run_command([self.SVN, 'diff', self.ROOT_URL, '-c', str(revision)])
+        if proc.returncode != 0:
+            return None
+        match = re.search(b'Merged (.*):r(\d+)-(\d+)', proc.stdout)
+        if match is None:
+            return None
+        return Mergeinfo(match.group(1).decode('utf-8'), int(match.group(2)), int(match.group(3)))
 
 
-def get_mergeinfo(revision: int) -> Optional[Mergeinfo]:
-    proc = run_command([SVN, 'diff', ROOT_URL, '-c', str(revision)])
-    if proc.returncode != 0:
-        return None
-    match = re.search(b'Merged (.*):r(\d+)-(\d+)', proc.stdout)
-    if match is None:
-        return None
-    return Mergeinfo(match.group(1).decode('utf-8'), int(match.group(2)), int(match.group(3)))
+    def get_inrange_revisions(self, mergeinfo: Mergeinfo) -> Optional[List[int]]:
+        proc = run_command([self.SVN, 'log', self.ROOT_URL + mergeinfo.path])
+        if proc.returncode != 0:
+            return None
+        nums = []
+        for line in proc.stdout.splitlines():
+            match = re.search(b'^r(\d+) \|', line)
+            if match:
+                nums.append(int(match.group(1)))
+        res = [x for x in nums if mergeinfo.revbegin <= x <= mergeinfo.revend]
+        res.reverse()
+        return res
 
 
-def get_inrange_revisions(mergeinfo: Mergeinfo) -> Optional[List[int]]:
-    proc = run_command([SVN, 'log', ROOT_URL + mergeinfo.path])
-    if proc.returncode != 0:
-        return None
-    nums = []
-    for line in proc.stdout.splitlines():
-        match = re.search(b'^r(\d+) \|', line)
-        if match:
-            nums.append(int(match.group(1)))
-    res = [x for x in nums if mergeinfo.revbegin <= x <= mergeinfo.revend]
-    res.reverse()
-    return res
+    def scan(self, since: int, to: int) -> None:
+        self.OUTDIR.mkdir(exist_ok=True)
+        ofile = (self.OUTDIR / f'merges{since}-{to}.txt').open('w')
 
+        for i in range(since, to+1):
+            print(f'trying {i}...')
+            mginfo = self.get_mergeinfo(i)
+            if mginfo is None:
+                continue
+            lst = self.get_inrange_revisions(mginfo)
+            ofile.write('{} path="{}" revbegin={} revend={} commits={}\n'
+                .format(i, mginfo.path, mginfo.revbegin, mginfo.revend,
+                    ",".join([str(x) for x in lst]) if lst else ''
+            ))
 
-def scan(since: int, to: int) -> None:
-    OUTDIR.mkdir(exist_ok=True)
-    ofile = (OUTDIR / f'merges{since}-{to}.txt').open('w')
-
-    for i in range(since, to+1):
-        print(f'trying {i}...')
-        mginfo = get_mergeinfo(i)
-        if mginfo is None:
-            continue
-        lst = get_inrange_revisions(mginfo)
-        ofile.write('{} path="{}" revbegin={} revend={} commits={}\n'
-            .format(i, mginfo.path, mginfo.revbegin, mginfo.revend,
-                ",".join([str(x) for x in lst]) if lst else ''
-        ))
-
-    ofile.close()
+        ofile.close()
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        arg1 = sys.argv[1].split('-')
-        since = int(arg1[0])
-        to = int(arg1[1])
+    parser = argparse.ArgumentParser('wram')
+    parser.add_argument('-e', '--executable', type=str, help='path to svn')
+    parser.add_argument('-u', '--url', required=True,  help='url to repository')
+    parser.add_argument('-r', '--range', type=str, help='revision range xx-yy')
+    args = parser.parse_args()
+
+    exe = args.executable or 'svn'
+    url = args.url
+    p = Program(exe, url)
+
+    rangestr = args.range
+    if rangestr is not None:
+        since, to = [int(x) for x in rangestr.split('-')]
     else:
-        since = 1
-        to = get_last_rev_number()
-    scan(since, to)
+        since, to = 1, p.get_last_rev_number()
+    p.scan(since, to)
